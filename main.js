2c910d7ec852ecf3f74db7dc32bfa63d2c435680
Khanh Chung <khanhchung1997123@gmail.com>
2021-02-28 11:34:02 -0800

    Add computerPlay()

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert("Connected");
+function computerPlay() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const index = Math.floor(Math.random() * Math.floor(choices.length));
+  return choices[index];
+}
